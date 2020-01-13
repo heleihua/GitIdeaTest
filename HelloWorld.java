94fe02caab1accd374fcc9b7658c4fc7caab14de
heleihua <36784877+heleihua@users.noreply.github.com>
2020-01-13 19:19:00 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+
+import java.util.Date;
+
+/**
+ * @created by Administrator at 2020-01-05 23:50
+ */
+public class HelloWorld {
+    public HelloWorld() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("HELLO World ");
+        Date date;
+        date = getDate();
+        System.out.println("date = " + date);
+        System.out.println(date);
+    }
+
+    private static Date getDate() {
+        return new Date();
+    }
+
+}
